40c2302229be4d3530474e28bf94dbfb33ca9df1
susmoysb <susmoysb@gmail.com>
2025-03-17 00:10:20 +0600

    comments.php created

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ It has several files containing different exercises, each focusing on a specific
 
 1. `header.php`: This file sets the output to plain text format. I include this file in every other file using the `require` statement.
 2. `syntax.php`: This file contains exercises related to PHP syntax.
+3. `comments.php`: This file contains exercises related to adding comments in PHP code.
---
+++ comments.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'header.php';
+
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/* This is a
+multi-line comment */
+
+// Outputs a welcome message:
+echo "Welcome Home!\n";
+
+echo "Welcome Home!\n"; // Outputs a welcome message
+
+// echo "Welcome Home!";
+
+/*
+The next statement will
+print a welcome message
+*/
+echo "Welcome Home!\n";
+
+/*
+echo "Welcome to my home!";
+echo "How are you?";
+*/
+echo "Hello!\n";
+
+$x = 5 /* + 15 */ + 5;
+echo $x . "\n"; // Output: 10
+
+?>
